27c8e988c5ece58d46765a00d0bdee1ec13dee2e
saimadhurig <81443037+saimadhurig@users.noreply.github.com>
2021-07-21 04:54:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,10 @@ def check_alarm_input(alarm_time):
         if alarm_time[0] < 24 and alarm_time[0] >= 0:
             return True
     if len(alarm_time) == 2: # [Hour:Minute] Format
-        if alarm_time[0] < 24 and alarm_time[0] >= 0 and 
-           alarm_time[1] < 60 and alarm_time[1] >= 0:
+        if alarm_time[0] < 24 and alarm_time[0] >= 0 and alarm_time[1] < 60 and alarm_time[1] >= 0:
             return True
-    elif len(alarm_time) == 3: // [Hour:Minute:Second] Format
-        if alarm_time[0] < 24 and alarm_time[0] >= 0 and 
-           alarm_time[1] < 60 and alarm_time[1] >= 0 and 
-           alarm_time[2] < 60 and alarm_time[2] >= 0:
+    elif len(alarm_time) == 3: # [Hour:Minute:Second] Format
+        if alarm_time[0] < 24 and alarm_time[0] >= 0 and alarm_time[1] < 60 and alarm_time[1] >= 0 and alarm_time[2] < 60 and alarm_time[2] >= 0:
             return True
     return False
   
@@ -34,7 +31,7 @@ def check_alarm_input(alarm_time):
 
 print("Set a time for the alarm (Ex. 06:30 or 18:30:00)")
 while True:
-    alarm_input = input(">> ")
+    alarm_input = input(">> ").split()
     try:
         alarm_time = [int(n) for n in alarm_input.split(":")]
         if check_alarm_input(alarm_time):
